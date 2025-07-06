fe42912976af2b7b6c018bfd8e7ce2c4ee44c619
Anika <anikao.luv@gmail.com>
2025-07-06 01:22:12 -0500

    added run script and included a way for the bot to create a local file *untested*

--- src/bot.ts
+++ src/bot.ts
@@ -2,10 +2,23 @@ import { Client } from "discord.js";
 import { config } from "./config";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
+import fs from "fs";
 
 const client = new Client({
   intents: [],
 });
+
+function createFileAsync(filename: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, "", (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+if (!fs.existsSync("userWwydPrompts.json"))
+  createFileAsync("userWwydPrompts.json");
 
 ready(client);
 interactionCreate(client);
